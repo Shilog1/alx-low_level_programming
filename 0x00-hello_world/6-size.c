5d2566ebb76482dd39eb2677c61694feecd1eff2
Shilog1 <ajulooluwaseunsamuel@gmail.com>
2022-10-17 02:14:49 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+*/
+int main (void)
+{
+	printf("size of a char: %: byte(s)\n", size of (char));
+	printf("size of a int: %: byte(s)\n", size of (int));
+	printf("size of a long int: %: byte(s)\n", size of (long int));
+	printf("size of a float: %: byte(s)\n", size of (float));
+	return (0);
+}
+
