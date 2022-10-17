d1c7c4ae0f279bfdd846f2f39a9f2e714447aef9
Shilog1 <ajulooluwaseunsamuel@gmail.com>
2022-10-17 03:49:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
- */
+* main - prints a string to standard output using puts function
+* Return: always 0
+*/
 int main(void)
-
 {
-	puts("programming is like building a multilingual puzzle,\n");
-	return (0);
-
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
