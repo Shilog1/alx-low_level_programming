e582aa662e7a68e1f201f13d646b4b28c718980d
Shilog1 <ajulooluwaseunsamuel@gmail.com>
2022-10-20 10:31:54 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - Alphabet in lowercase and uppercase
+* Return: Always (Success)
+*/
+int main(void)
+{
+		char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+						putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+							putchar(c);
+
+					putchar('\n');
+						return (0);
+}
